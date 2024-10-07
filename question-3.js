fadcabccb01c64c2c3faeb567628a22577488885
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-07 13:29:04 +0700

    completed question 3

--- question-3.js
+++ question-3.js
@@ -1,3 +1,15 @@
 // Question #3
-let userPassword = "";
+let userPassword = "password";
 // เริ่มเขียนโค้ดตรงนี้
+
+function checkPasswordStrength(password) {
+  if (password.length > 10) {
+    return "Strong";
+  } else if (password.length >= 6 && password.length <= 10) {
+    return "Medium";
+  } else if (password.length < 6) {
+    return "Weak";
+  }
+}
+
+console.log(checkPasswordStrength(userPassword));
