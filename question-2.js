a87730f8d8c76a4c3d0816a4f8c47c760b6a0da3
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-07 13:19:39 +0700

    completed question 2

--- question-2.js
+++ question-2.js
@@ -4,3 +4,12 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity = 200;
+inventory[2] = { name: "Orange", price: 20, quantity: 300 };
+const appleValue = inventory[0].price * inventory[0].quantity;
+const bananaValue = inventory[1].price * inventory[1].quantity;
+const orangeValue = inventory[2].price * inventory[2].quantity;
+const totalValue = appleValue + bananaValue + orangeValue;
+
+console.log(inventory);
+console.log(totalValue);
