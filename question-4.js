c1eb9d81f0ae640b89062add249bf118c3ed141a
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-07 13:38:23 +0700

    completed question 4

--- question-4.js
+++ question-4.js
@@ -5,3 +5,16 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+function findLowQuant(arr) {
+  let lowestName;
+  let lowestQuantity = arr[0].quantity;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].quantity < lowestQuantity) {
+      lowestQuantity = arr[i].quantity;
+      lowestName = arr[i].name;
+    }
+  }
+  return `${lowestName} has the lowest quantity at ${lowestQuantity}`;
+}
+
+console.log(findLowQuant(inventory));
