e9439f0731b171c8ea33f454431f3622a05f8bc6
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-07 13:51:06 +0700

    completed question 5

--- question-5.js
+++ question-5.js
@@ -19,3 +19,27 @@ const products = [
 
 const promotionCode = "";
 // เริ่มเขียนโค้ดตรงนี้
+function calculateTotalPrice(product, discount) {
+  let totalPrice = 0;
+  let totalDiscount = 0;
+  for (let item of product) {
+    totalPrice += item.price * item.quantity;
+  }
+  switch (discount) {
+    case "SALE20":
+      totalDiscount = 0.8;
+      break;
+
+    case "SALE50":
+      totalDiscount = 0.5;
+      break;
+
+    default:
+      totalDiscount = 1;
+      break;
+  }
+
+  return totalPrice * totalDiscount;
+}
+
+console.log(calculateTotalPrice(products, promotionCode));
